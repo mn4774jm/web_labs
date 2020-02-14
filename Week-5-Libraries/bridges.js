d6547aa0302b63383e89edbb59e562832e2c93d7
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-02-14 14:10:09 -0600

    Bridges functioning as intended. Need to comment code in all programs and add links to index file.

--- Week-5-Libraries/bridges.js
+++ Week-5-Libraries/bridges.js
@@ -14,11 +14,11 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 
 bridge_obj = [
-    {'bridge_name': 'Varrazano-Narrows Bridge', 'city_state:': 'New York, NY', 'Span': 'Span: 1298.4 meters', 'coordinates': [40.6066, -74.0447] },
-    {'bridge_name': 'Golden Gate Bridge', 'city_state:': 'San Francisco and Marin, CA', 'Span': 'Span: 1280.2 meters', 'coordinates': [37.8199, -122.4783] },
-    {'bridge_name': 'Mackinac Bridge', 'city_state:': 'Mackinaw and St. Ignace, MI', 'Span': 'Span: 1158.0 meters', 'coordinates': [45.8174, -84.7278] },
-    {'bridge_name': 'George Washington Bridge', 'city_state:': 'New York, NY and New Jersey, NJ', 'Span': 'Span: 1067.0 meters', 'coordinates': [40.8517, -73.9527] },
-    {'bridge_name': 'Tacoma Narrows Bridge', 'city_state:': 'Tacoma and Kitsap, WA', 'Span': 'Span: 853.44 meters', 'coordinates': [47.2690, -122.5517] },
+    {'bridge_name': 'Varrazano-Narrows Bridge', 'city_state:': 'New York, NY', 'Span': '1298.4', 'coordinates': [40.6066, -74.0447] },
+    {'bridge_name': 'Golden Gate Bridge', 'city_state:': 'San Francisco and Marin, CA', 'Span': '1280.2', 'coordinates': [37.8199, -122.4783] },
+    {'bridge_name': 'Mackinac Bridge', 'city_state:': 'Mackinaw and St. Ignace, MI', 'Span': '1158.0', 'coordinates': [45.8174, -84.7278] },
+    {'bridge_name': 'George Washington Bridge', 'city_state:': 'New York, NY and New Jersey, NJ', 'Span': '1067.0', 'coordinates': [40.8517, -73.9527] },
+    {'bridge_name': 'Tacoma Narrows Bridge', 'city_state:': 'Tacoma and Kitsap, WA', 'Span': '853.44', 'coordinates': [47.2690, -122.5517] },
 ];
 
 for (let x = 0; x < bridge_obj.length;x++){
@@ -26,17 +26,21 @@ for (let x = 0; x < bridge_obj.length;x++){
 
 };
 
+
+
 let canvas = document.getElementById('bridge_chart');
 console.log(canvas);
 let ctx = canvas.getContext('2d')
 
 chart = new Chart(ctx, {
+
     type: 'bar',
     data: {
-        labels: ['Coke', 'Pepsi', 'Either', 'Neither'],
+
+        labels: [],
         datasets: [{
-            label: 'Number of Votes',
-            data: [18, 14, 7, 10],
+            label: 'Length in Meters',
+            data: [],
             backgroundColor: ['red', 'blue', 'yellow', 'green']
         }]
     }, options: {
@@ -48,5 +52,12 @@ chart = new Chart(ctx, {
             }]
         }
     }
-})
+});
+
+for (let x = 0; x < bridge_obj.length;x++){
+    chart.data.labels.push(bridge_obj[x].bridge_name)
+    chart.data.datasets[0].data.push(bridge_obj[x].Span)
+}
+
+
 
