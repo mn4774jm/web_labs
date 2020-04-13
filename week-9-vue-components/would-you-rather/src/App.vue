dc75ac62c78d156d6e14be242edcc25b14f55894
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-04-13 15:20:19 -0500

    Beginning lab 9

---
+++ week-9-vue-components/would-you-rather/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <WouldYouRather></WouldYouRather>
+  </div>
+
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      questions: [{
+        id:0,
+        question: '...be a master at drawing or be an amazing singer?',
+        answer1: "Drawing",
+        answer2: "Singing",
+      },
+        {
+          id:1,
+          question: '...be a wizard or a superhero?',
+          answer1: 'Be a wizard',
+          answer2: 'Be a superhero'
+        },
+        {
+          id:2,
+          question: '... work alone on a school project or work with others on a school project?',
+          answer1: 'Alone',
+          answer2: 'Group project'
+        }
+      ]
+    }
+  },
+  components: {
+    WouldYouRather
+  }
+}
+</script>
+
+<style>
+#app {
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ week-9-vue-components/would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "WouldYouRather"
+    }
+</script>
+
+<style scoped>
+
+</style>
