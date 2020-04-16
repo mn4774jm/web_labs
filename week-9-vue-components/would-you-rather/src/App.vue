bddab94ac28803d81a52005054a92e5f5984c465
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-04-16 14:02:48 -0500

    Labs completed. Still need to add some comments

--- week-9-vue-components/would-you-rather/src/App.vue
+++ week-9-vue-components/would-you-rather/src/App.vue
@@ -1,17 +1,32 @@
+
 <template>
+
   <div id="app">
-    <WouldYouRather></WouldYouRather>
+<!--    loop through data for each question -->
+    <WouldYouRather v-for="question in questions" v-bind:key="question.id"
+      v-bind:id="id"
+      v-bind:question="question"
+      v-bind:answer1="answer1"
+      v-bind:answer2="answer2"
+      v-on:answer-changed="answerChanged"
+    ></WouldYouRather>
+<!--    header and list for user answers-->
+    <h1>Would you Rather...</h1>
+<ul id="answerList"></ul>
   </div>
 
 </template>
 
 <script>
+  // import component file(s)
 import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'app',
+  // data function and new list to hold user answer selections
   data() {
     return {
+      answers: [],
       questions: [{
         id:0,
         question: '...be a master at drawing or be an amazing singer?',
@@ -33,8 +48,35 @@ export default {
       ]
     }
   },
+  // props to pass information child
+  props: {
+    id: String,
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  // call for component
   components: {
     WouldYouRather
+  },
+  methods: {
+    // pass user id, and user choice as answer
+    // get list item using id as key
+    answerChanged(id, answer) {
+      this.answers[id] = answer
+      this.buildList()
+    },
+    buildList() {
+      // find element
+      let list = document.getElementById('answerList');
+      list.innerHTML = '';
+      // loop through answers and update list after each change
+      this.answers.forEach(function(answer) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = answer;
+        list.appendChild(listItem)
+      })
+    }
   }
 }
 </script>
--- week-9-vue-components/would-you-rather/src/components/WouldYouRather.vue
+++ week-9-vue-components/would-you-rather/src/components/WouldYouRather.vue
@@ -1,13 +1,38 @@
 <template>
-    
+    <div class="question">
+<!--        write template here-->
+        <h3>Would you Rather...</h3>
+
+        <h2>{{ question.question }}</h2>
+        <input type="radio" v-model="choice" v-bind:value="question.answer1" v-on:change="answerChanged(question.id, choice)">
+        <label>{{ question.answer1 }}</label>
+        <input type="radio" v-model="choice" v-bind:value="question.answer2" v-on:change="answerChanged(question.id, choice)">
+        <label>{{ question.answer2 }}</label>
+    </div>
 </template>
 
 <script>
+    // create and export component here
     export default {
-        name: "WouldYouRather"
+        name: "WouldYouRather",
+
+    data() {
+      return {
+          choice: ''
+      }
+    },
+        props: {
+        question: Object,
+        },
+    methods: {
+        answerChanged(id, choice) {
+        this.$emit('answer-changed', id, choice)
+        }
     }
+    }
+
 </script>
 
-<style scoped>
+<style>
 
 </style>
